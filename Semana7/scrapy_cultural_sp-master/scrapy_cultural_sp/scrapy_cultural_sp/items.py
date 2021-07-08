01e0ae138654817146358349a650f4ef7a639dd5
Karen Benjamim <73201520+KarenBenjamim@users.noreply.github.com>
2021-07-08 16:12:04 -0300

    Delete Semana7/scrapy_cultural_sp-master directory

--- Semana7/scrapy_cultural_sp-master/scrapy_cultural_sp/scrapy_cultural_sp/items.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from scrapy.item import Item, Field
-
-class Atracao(Item):
-    cidade = Field()
-    endereco = Field()
-    dia = Field()
-    hora = Field()
-    artista = Field()
